043043604eccd00ed688220ffc17e1e221e7aa63
Eugene Molotov <molotov@it-projects.info>
2021-09-28 00:54:59 +0500

    Добавил скрипт для клонирования шаблона проекта
    Чтобы не было необходимости печатать очень длинную команду

---
+++ 13.0/projects/scaffold.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+git clone -b 13.0 --single-branch -o upstream https://github.com/em230418/odoo-project-template.git $@
---
+++ 14.0/projects/scaffold.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+git clone -b 14.0 --single-branch -o upstream https://github.com/em230418/odoo-project-template.git $@
