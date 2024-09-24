6bcb4002b914563165c30b33b5650c9123f5afef
Eugene Molotov <molotov@it-projects.info>
2024-09-24 16:32:50 +0500

    17.0: добавил скрипт для разворачивания

---
+++ 17.0/projects/scaffold.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+git clone -b 17.0 --single-branch -o upstream https://github.com/em230418/odoo-project-template.git $@
+cd $@
+git remote add origin git@github.com:em230418/odoo-project-template.git
